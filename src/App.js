6970e2fc9723ceacc4d3dbac3e12298e103e5c54
Keone Martin <keonemartin@gmail.com>
2018-02-11 21:41:06 +1100

    added .env file to gitignore

--- src/App.js
+++ src/App.js
@@ -1,18 +1,117 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import uuid from 'uuid';
+import firebase from 'firebase';
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      id: uuid.v1(),
+      name: '',
+      answers: {
+        q1: '',
+        q2: '',
+        q3: '',
+        q4: ''
+      },
+      submitted: false
+    };
+    this.handleQuestionChange = this.handleQuestionChange.bind(this)
+  }
+
+  handleNameSubmit(event){
+    var name = this.refs.name.value;
+    this.setState({ name: name }, function(){
+      console.log(this.state);
+    });
+    event.preventDefault();
+  }
+
+  handleQuestionSubmit(event){
+    console.log();
+  }
+
+  handleQuestionChange(event){
+    var question = event.target.name;
+    var answers = this.state.answers;
+    answers[question] = event.target.value;
+
+    this.setState({answers: answers}, function(){
+      console.log(this.state);
+    });
+  }
+
   render() {
+    var user;
+    var questions;
+    if(this.state.name && this.state.submitted === false){
+      user = <h2>Welcome {this.state.name}</h2>;
+      questions = (
+        <span>
+          <h3>Survey Questions</h3>
+          <form onSubmit={this.handleQuestionSubmit.bind(this)}>
+            <div>
+              <label>What is your favourite operating system?</label><br/>
+              <input type="radio" name="q1" value="Windows" onChange={this.handleQuestionChange}/>Windows<br/>
+              <input type="radio" name="q1" value="OSX" onChange={this.handleQuestionChange}/>OSX<br/>
+              <input type="radio" name="q1" value="Linux" onChange={this.handleQuestionChange}/>Linux<br/>
+              <input type="radio" name="q1" value="Solaris" onChange={this.handleQuestionChange}/>Solaris<br/>
+              <input type="radio" name="q1" value="Other" onChange={this.handleQuestionChange}/>Other<br/>
+            </div>
+            <div>
+              <label>What is your favourite brand?</label><br/>
+              <input type="radio" name="q2" value="Sony" onChange={this.handleQuestionChange}/>Sony<br/>
+              <input type="radio" name="q2" value="Samsung" onChange={this.handleQuestionChange}/>Samsung<br/>
+              <input type="radio" name="q2" value="Panasonic" onChange={this.handleQuestionChange}/>Panasonic<br/>
+              <input type="radio" name="q2" value="Vizio" onChange={this.handleQuestionChange}/>Vizio<br/>
+              <input type="radio" name="q2" value="Other" onChange={this.handleQuestionChange}/>Other<br/>
+            </div>
+            <div>
+              <label>What is your favourite Smartphone brand?</label><br/>
+              <input type="radio" name="q3" value="Samsung" onChange={this.handleQuestionChange}/>Samsung<br/>
+              <input type="radio" name="q3" value="Apple" onChange={this.handleQuestionChange}/>Apple<br/>
+              <input type="radio" name="q3" value="Nexus" onChange={this.handleQuestionChange}/>Nexus<br/>
+              <input type="radio" name="q3" value="Blackberry" onChange={this.handleQuestionChange}/>Blackberry<br/>
+              <input type="radio" name="q3" value="Other" onChange={this.handleQuestionChange}/>Other<br/>
+            </div>
+            <div>
+              <label>What is your favourite CPU brand?</label><br/>
+              <input type="radio" name="q4" value="Intel" onChange={this.handleQuestionChange}/>Intel<br/>
+              <input type="radio" name="q4" value="AMD" onChange={this.handleQuestionChange}/>AMD<br/>
+              <input type="radio" name="q4" value="Nvidia" onChange={this.handleQuestionChange}/>Nvidia<br/>
+              <input type="radio" name="q4" value="ARM" onChange={this.handleQuestionChange}/>ARM<br/>
+              <input type="radio" name="q4" value="Other" onChange={this.handleQuestionChange}/>Other<br/>
+            </div>
+          </form>
+        </span>
+      )
+
+    } else if(!this.state.name && this.state.submitted === false) {
+      user = (
+        <span>
+          <h2>Please enter your name to begin the survery</h2>
+          <form onSubmit={this.handleNameSubmit.bind(this)}>
+            <input type="text" placeholder="Enter Name..." ref="name"/>
+          </form>
+        </span>
+      )
+
+    } else if(this.state.submitted === true){
+
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header text-center">
+          <h1 className="App-title">Simple Survey</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="text-center">
+          {user}
+        </div>
+        <div className="container">
+          {questions}
+        </div>
       </div>
     );
   }
